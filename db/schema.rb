0830a9d885b6b956ebb4476737dd639fe62f3e9e
bennypaulino <santiagopro71@gmail.com>
2016-06-04 16:12:44 -0400

    Create an Image model and set up associations between it and the Installation model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604195804) do
+ActiveRecord::Schema.define(version: 20160604200713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.text     "caption"
+    t.integer  "art_installation_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "photo"
+  end
 
   create_table "installations", force: :cascade do |t|
     t.string   "name"
