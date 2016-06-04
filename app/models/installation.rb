a56a7ebf9aaab1ae046ed3e8f6db8cb069a45d78
bennypaulino <santiagopro71@gmail.com>
2016-06-04 16:01:37 -0400

    Add a model for tags

--- app/models/installation.rb
+++ app/models/installation.rb
@@ -1,2 +1,7 @@
 class Installation < ActiveRecord::Base
+  has_many :tags
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true
+  validates :address, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604172355) do
+ActiveRecord::Schema.define(version: 20160604195804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20160604172355) do
     t.datetime "updated_at",  null: false
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
