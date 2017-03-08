e7e4c28e2e6637458abdfc6b0bb2dc90ed4dc3ad
Jimmy Reichley <jimmyqpublik@gmail.com>
2017-03-08 01:55:40 -0500

    Use local image uploads folder instead of s3 if not in production

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -9,7 +9,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-  storage :aws
+  storage ( Rails.env.production? ? :aws : :file )
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
